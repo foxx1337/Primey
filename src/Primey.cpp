f9386c9ed5392c91caeb7ba9d7d12504429d3c74
foxx1337 <foxx1337@yahoo.com>
2024-11-05 16:25:17 +0100

    Check the global stop fewer times

--- src/Primey.cpp
+++ src/Primey.cpp
@@ -6,24 +6,40 @@
 
 std::atomic_bool stop;
 
-
 std::vector<unsigned long long> primes;
+
+unsigned long long loops = 0;
 
 void primes_worker()
 {
     primes.push_back(2);
     unsigned long long n = 3;
-    while (!stop)
+    bool ready = true;
+    unsigned long long ops = 0;
+    while (ready)
     {
         bool is_prime = true;
         for (unsigned long long i = 2; i * i <= n; i++)
         {
+            ops += 1;
             if (n % i == 0)
             {
                 is_prime = false;
                 break;
             }
+
+            if (ops >= 10'000'000)
+            {
+                loops += ops;
+                ops = 0;
+                if (stop)
+                {
+                    ready = false;
+                    break;
+                }
+            }
         }
+
         if (is_prime)
         {
             primes.push_back(n);
@@ -56,6 +72,8 @@ int main() {
     progress_sleep(5s);
     stop = true;
 
-    std::cout << "Found " << primes.size() << " primes." << std::endl;
+    std::cout << "Found " << primes.size() << " primes.\n";
+    std::cout << "Loops: " << loops << '\n';
+    std::cout << "Last prime: " << primes.back() << std::endl;
     return 0;
 }
